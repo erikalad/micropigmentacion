b2f150ab75f20760d577d050ee1b4c239955e656
Erika Ladner <erikaladner@MacBook-Air-de-Erika.local>
2024-03-27 13:06:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Componentes/Home';
+import Section2 from './Componentes/Section2';
+import Section3 from './Componentes/Section3';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
+      <Section2/>
+      <Section3/>
     </div>
   );
 }
---
+++ src/Componentes/Home.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './home.scss'
+
+export default function Home() {
+  return (
+    <div>
+    <section class="home">
+      <div class="description">
+        <h1 class="title">
+        
+
+          <span class="gradient-text">Latín Academy </span> school lips online
+        </h1>
+        <p class="paragraph">
+          In a world filled with opportunities, having a mentor can make all the
+          difference. Explore why people turn to this invaluable resource to
+          unlock their potential.
+        </p>
+      </div>
+
+      <div class="users-color-container">
+        <span class="item" ></span>
+        <img
+          class="item"
+          src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/274f29ce-0d3f-4ac2-a2aa-f9b7bd188b2a"
+          
+          alt="" />
+        <span class="item" ></span>
+        <img
+          class="item"
+          src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/b8a14493-3d9f-4b9b-b93a-56d0bc7243e9"
+          
+          alt="" />
+
+        <img
+          class="item"
+          src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/03e51e1e-9750-45a5-b75e-a1e341d4562a"
+         
+          alt="" />
+        <span class="item"></span>
+        <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/5eb50f89-3e5a-480e-860c-8d40d3ba9ffe" alt="" />
+        <span class="item"></span>
+
+        <span class="item" ></span>
+        <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/86c71a79-2efe-4567-8665-b1e5a1fd9735" alt="" />
+        <span class="item" ></span>
+        <img class="item" src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/97ef9643-5202-41aa-80f0-ceeabccdd099" alt="" />
+      </div>
+    </section>
+
+
+    </div>
+  )
+}
